baa6b5d0c127e19e1236d80db48b70035b5eed85
Kaycaul <riley.robertson5@gmail.com>
2023-03-12 15:53:01 -0400

    Co-authored-by: Duncan Stinson <DuncanStinson@users.noreply.github.com>

---
+++ PathLine.gd
@@ -0,0 +1,10 @@
+extends Line2D
+
+# draws a visible line over the path that the player follows
+onready var parent_path = get_parent()
+
+func _ready():
+	# iterate over every point in the invisible player path
+	for point in parent_path.curve.get_baked_points():
+		add_point(point + position) # points are relative to the paths location, so add position
+
