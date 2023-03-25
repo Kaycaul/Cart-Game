0ccdb778e2ed73d1fd8e0aa17191167a6347a1a1
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-03-25 11:58:17 -0400

    cleaned up player file

--- Player.gd
+++ Player.gd
@@ -2,11 +2,11 @@ extends Area2D
 
 onready var path_follow = get_parent() # gets the parent (which should be a path follower) to move the player
 
-export var move_speed = 1
 export var accel = 0.5
 export var MAX_SPEED = 601
 export var DEFAULT_SPEED = 100
 
+var move_speed = DEFAULT_SPEED
 var side_movement = 0
 
 func _process(_delta):
@@ -25,20 +25,6 @@ func _physics_process(delta):
 	if move_speed < MAX_SPEED:
 		move_speed += accel
 	
-	if move_speed == 600:
-		print("I AM SPEED ")
-		$RemoteCartCamera.position.x += 2.5
-	elif move_speed == 500: 
-		print("I AM MODERATE SPEED ")
-		$RemoteCartCamera.position.x += 2.5
-	elif move_speed == 400: 
-		print("I AM GOING FAST ")
-		$RemoteCartCamera.position.x += 2.5
-	elif move_speed == 300: 
-		print("WHAT SPEED LIMIT? ")
-		$RemoteCartCamera.position.x += 2.5
-	else: 
-		print("Slow driver")
 	# move side/side on the path (up and down on the path)
 	position.y = side_movement
 
@@ -46,15 +32,11 @@ func _physics_process(delta):
 func collideWithObstacle():
 	$CollisionEffect.play()
 	print ("in here 2")
-	var currentOff = path_follow.get_offset()
 	print(path_follow.get_offset())
 	position.x = position.x - 50
 	if((floor(move_speed / 100)) - 2 > 0):
 		$RemoteCartCamera.position.x -= 2.5 * ((floor(move_speed / 100)) - 2)
 	print("entering wait")
-
-	move_speed = 0
-	accel = 0
 	
 	#play skid animation
 	$AnimationPlayer.play("skid")
@@ -65,8 +47,6 @@ func collideWithObstacle():
 	print("exited wait")
 
 
-func _on_PlayerObject_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	print(area)
+func _on_PlayerObject_area_shape_entered(_area_rid, area, _area_shape_index, _local_shape_index):
 	if(str(area).split(":")[0].rstrip("0123456789@").lstrip("@") == "Obstacle"):
 		collideWithObstacle()
-	pass # Replace with function body.
