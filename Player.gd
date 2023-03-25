a328f57bde78e8df8d0601f600fdcb9f49b3c7de
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-03-25 12:01:44 -0400

    more cleanup

--- Player.gd
+++ Player.gd
@@ -21,7 +21,6 @@ func _process(_delta):
 func _physics_process(delta):
 	# moves along the path by a certain speed
 	path_follow.set_offset(path_follow.get_offset() + move_speed * delta) 
-	print(move_speed)
 	if move_speed < MAX_SPEED:
 		move_speed += accel
 	
@@ -31,12 +30,9 @@ func _physics_process(delta):
 
 func collideWithObstacle():
 	$CollisionEffect.play()
-	print ("in here 2")
-	print(path_follow.get_offset())
 	position.x = position.x - 50
 	if((floor(move_speed / 100)) - 2 > 0):
 		$RemoteCartCamera.position.x -= 2.5 * ((floor(move_speed / 100)) - 2)
-	print("entering wait")
 	
 	#play skid animation
 	$AnimationPlayer.play("skid")
@@ -44,7 +40,6 @@ func collideWithObstacle():
 	position.x = position.x + 50
 	accel = 0.5
 	move_speed = DEFAULT_SPEED	
-	print("exited wait")
 
 
 func _on_PlayerObject_area_shape_entered(_area_rid, area, _area_shape_index, _local_shape_index):
