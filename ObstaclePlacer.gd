052780eebe2827ebdae868a379169b42282535a0
Isabelle Wang <84156293+wang-i@users.noreply.github.com>
2023-03-28 18:27:56 -0400

    i had an epiphany

--- ObstaclePlacer.gd
+++ ObstaclePlacer.gd
@@ -7,7 +7,7 @@ export var spawner_seed = "200"
 func _ready():
 	seed(spawner_seed.hash())
 	for _i in range(obstacle_count):
-		spawn_obstacle(randf(), 20 * ((randi() % 5)-2))
+		spawn_obstacle(randf(), 15 * ((randi() % 5)-2))
 
 func spawn_obstacle(offset, lane):
 	# move along the path to the given position
@@ -15,9 +15,5 @@ func spawn_obstacle(offset, lane):
 	# spawn the obstacle, but dont parent it to this
 	var obstacle = obstacle_to_spawn.instance()
 	get_node("/root").call_deferred("add_child", obstacle) # error handler told me to use call_deferred, fixed everything, idk why
-	print("position ", position)
-	obstacle.position = position + (lane * Vector2.RIGHT)
-	print("lane ", lane)
-	print("after ", obstacle.position)
-	print(" ")
+	obstacle.position = position + (lane * Vector2(1, 0))
 	obstacle.rotation = rotation
