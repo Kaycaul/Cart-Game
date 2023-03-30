892494c592f5cec51b360fba4c332d8564b606a9
Isabelle Wang <84156293+wang-i@users.noreply.github.com>
2023-03-30 12:50:18 -0400

    anchored timer label and decreased obstacle and coin count

--- Scripts/TimerLabel.gd
+++ Scripts/TimerLabel.gd
@@ -1,21 +1,11 @@
 extends Label
 
 var time = 0
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var Transform2d = get_viewport().get_canvas_transform()
 	get_viewport().set_canvas_transform(Transform2d)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Timer_timeout():
 	time +=1 
@@ -29,5 +19,3 @@ func _on_Timer_timeout():
 		displayTime = (str(mins)+ ":" + str(sec))
 	
 	text = displayTime
-	print(text)
-
